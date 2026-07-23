aec703e9359457df05b3a81a5a722db7c49de866
gitmobkab <richmondaurel77@gmail.com>
2026-07-23 01:57:12 +0000

    fix: update database client initialization to use environment configuration

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,7 +1,8 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "../db/schema";
+import env from "./env"
 
-const client = postgres(process.env.DATABASE_URL!);
-
+// Disable prefetch as it is not supported for "Transaction" pool mode
+const client = postgres(env.DATABASE_URL, {prepare: false});
 export const db = drizzle(client, { schema });
