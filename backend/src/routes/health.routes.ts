9ba176d3ba5599d8e987f107497ad29fa480b5ff
gitmobkab <richmondaurel77@gmail.com>
2026-07-23 02:17:44 +0000

    style: format code for consistency in health and users routes

--- backend/src/routes/health.routes.ts
+++ backend/src/routes/health.routes.ts
@@ -3,13 +3,13 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/health", (_req, res) => {
-  res.json({ status: "ok" });
+	res.json({ status: "ok" });
 });
 
 router.post("/exemple", (req, res) => {
-  const io = req.app.get("io");
-  io.emit("message", "Événement depuis une route HTTP");
-  res.send("OK");
+	const io = req.app.get("io");
+	io.emit("message", "Événement depuis une route HTTP");
+	res.send("OK");
 });
 
 export default router;
--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
-import supabase from '../supabaseClient';
+import supabase from "../supabaseClient";
+
 const router = Router();
 
+router.get("/users", async (_req, res) => {
+	const { data, error } = await supabase.from("utilisateur").select("*");
 
-router.get('/users', async (_req, res) => {
-  const { data, error } = await supabase.from('utilisateur').select('*');
-  
-  if (error) {
-    return res.status(500).json({ message: 'Erreur de connexion', error: error.message });
-  }
-  
-  res.json({ message: 'Connexion réussie!', data })
+	if (error) {
+		return res
+			.status(500)
+			.json({ message: "Erreur de connexion", error: error.message });
+	}
+
+	res.json({ message: "Connexion réussie!", data });
 });
 
 export default router;
