52a814ebc1ad21a54e290d4e5311767f32f43e65
gitmobkab <richmondaurel77@gmail.com>
2026-07-28 01:31:07 +0000

    fix: update connection string to use env configuration

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,8 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import env from "../config/env";
 import * as schema from "./schema";
 
-const connectionString = process.env.DATABASE_URL as string;
+const connectionString = env.DATABASE_URL
 
 const client = postgres(connectionString);
 export const db = drizzle(client, { schema });
