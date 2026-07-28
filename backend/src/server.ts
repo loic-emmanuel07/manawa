58677e8b73d2555cfb4522afd953e1c679a37f2a
gitmobkab <richmondaurel77@gmail.com>
2026-07-28 01:05:57 +0000

    refactor: simplify socket server setup and remove unused code

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,27 +1,14 @@
-import "dotenv/config";
 import { createServer } from "node:http";
-import { Server } from "socket.io";
+import env from "./config/env";
 import app from "./app";
 
+import { createSocketServer } from "./sockets";
+
 const server = createServer(app); // on greffe le serveur HTTP sur votre app Express existante
-const io = new Server(server); // Socket.IO se greffe sur le serveur HTTP
-
-// Gestion des connexions Socket.IO
-io.on("connection", (socket) => {
-	console.log("Un client est connecté:", socket.id);
-
-	socket.on("disconnect", () => {
-		console.log("Client déconnecté");
-	});
-
-	socket.on("message", (data) => {
-		console.log("Message reçu:", data);
-		io.emit("message", data); // rediffuse à tous les clients
-	});
-});
+const io = createSocketServer(); // Socket.IO se greffe sur le serveur HTTP
 
 // On écoute sur "server", pas sur "app"
-const PORT = process.env.PORT || 3000;
+const PORT = env.PORT
 server.listen(PORT, () => {
 	console.log(`Serveur démarré sur le port ${PORT}`);
 });
