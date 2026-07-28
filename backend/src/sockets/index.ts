a3ffae6db91ecd9acb11903b92a234c17b9c34e7
gitmobkab <richmondaurel77@gmail.com>
2026-07-28 00:55:40 +0000

    register middlewares in socket server creation

--- backend/src/sockets/index.ts
+++ backend/src/sockets/index.ts
@@ -1,8 +1,10 @@
 import { Server } from "socket.io";
 import { registerHandlers } from "./handlers";
+import registerMiddlewares  from "./middlewares";
 
 export function createSocketServer(): Server{
     const io = new Server;
+    registerMiddlewares(io);
     registerHandlers(io);
     return io;
 }
