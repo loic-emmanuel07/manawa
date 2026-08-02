25a108ccfa85acb72a0cf215967f53a366ec3b03
gitmobkab <richmondaurel77@gmail.com>
2026-08-02 09:48:22 +0000

    fix: configure CORS middleware to use CORS_ORIGIN from environment variables

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import cors from "cors";
 import express from "express";
+import env from "./config/env";
 import authRoutes from "./routes/auth.routes";
 import healthRoutes from "./routes/health.routes";
 import userRoutes from "./routes/users.routes";
@@ -7,7 +8,7 @@ import userRoutes from "./routes/users.routes";
 const app = express();
 
 // MIDDLEWARES
-app.use(cors());
+app.use(cors({ origin: env.CORS_ORIGIN }));
 app.use(express.json());
 
 // ROUTES
